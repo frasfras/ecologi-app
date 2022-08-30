957d91187f4b127d91ae17d81f734ca632d0d675
frasfras <65541080+frasfras@users.noreply.github.com>
2022-08-30 14:47:14 +0800

    Add files via upload

---
+++ dist/js/about.811908ce.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkappcarbon"]=self["webpackChunkappcarbon"]||[]).push([[443],{5399:function(n,t,u){u.r(t),u.d(t,{default:function(){return l}});var a=function(){var n=this;n._self._c;return n._m(0)},s=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],r=u(1001),e={},c=(0,r.Z)(e,a,s,!1,null,null,null),l=c.exports}}]);
+//# sourceMappingURL=about.811908ce.js.map
